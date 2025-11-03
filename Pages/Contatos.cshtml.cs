7a7cd104d477a9caa7371ba80d6f1e74e0d9d865
juliareinke <juliareinke29@icloud.com>
2025-11-03 13:43:22 -0300

    Adiciona páginas Colabore e Contatos; realiza ajustes de responsividade e corrige faixa branca lateral em telas menores

---
+++ Pages/Contatos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PSE_ICEP.Pages
+{
+    public class ContatosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
